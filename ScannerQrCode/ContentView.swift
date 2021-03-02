42ea45f750763110a155300ac6859f4a239f7b49
Elechim01 <mikymanny015@gmail.com>
2021-03-02 10:15:15 +0100

    primo commit

--- ScannerQrCode/ContentView.swift
+++ ScannerQrCode/ContentView.swift
@@ -6,12 +6,32 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ContentView: View {
+    @State var isShowingScanner = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            self.isShowingScanner = true
+        }, label: {
+            Text("Mostra Scanner")
+        })
+        .sheet(isPresented: $isShowingScanner, content: {
+            CodeScannerView(codeTypes: [.qr], simulatedData: "Some simulated data",completion: self.handleScan)
+            })
     }
+    private func handleScan(result: Result<String, CodeScannerView.ScanError>) {
+           self.isShowingScanner = false
+        switch result{
+        case .success(let data):
+            print("Sucess with \(data)")
+            UIApplication.shared.open(URL(string: data)!)
+            
+        case .failure(let error):
+            print("Sucess with \(error)")
+        }
+    }
+   
 }
 
 struct ContentView_Previews: PreviewProvider {
