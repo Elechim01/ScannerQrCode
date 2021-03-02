e3c944bd33dccb87233e8c2dc77f52b11a728b35
Elechim01 <mikymanny015@gmail.com>
2021-03-02 09:32:24 +0100

    Initial Commit

---
+++ ScannerQrCode/ScannerQrCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerQrCodeApp.swift
+//  ScannerQrCode
+//
+//  Created by Michele Manniello on 02/03/21.
+//
+
+import SwiftUI
+
+@main
+struct ScannerQrCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
