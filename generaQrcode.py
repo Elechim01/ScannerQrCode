a644005a1a3ebb8d0ea3981a75e4569c9087576e
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-03-02 10:18:29 +0100

    Update generaQrcode.py

--- generaQrcode.py
+++ generaQrcode.py
@@ -4,18 +4,6 @@ Spyder Editor
 
 This is a temporary script file.
 """
-"""
-from covid  import Covid
-covid = Covid()
-print(covid.get_total_active_cases())
-print(covid.get_total_deaths())
-print(covid.get_total_confirmed_cases())
-
-import socket 
-hostin = socket.gethostname()
-IPAd = socket.gethostbyname(hostin)
-print("IP ADDRESs IS "+ IPAd)
-"""
 " generazione qrcode"
 import pyqrcode
 import png
